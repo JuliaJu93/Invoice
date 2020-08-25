688d4223f9cc0a6cfcfb34522898698e6671d84e
Julia Yusupova <cashleva.iulia@gmail.com>
2020-08-25 15:03:11 +0300

    Added css for page terminal

--- src/pages/terminals/components/terminals_table/terminals_table.js
+++ src/pages/terminals/components/terminals_table/terminals_table.js
@@ -7,12 +7,6 @@ function TerminalsTable({terminal, setTerminal}) {
 	return (
 		<div className = "terminal_table">
 			<table>
-				<thead>
-			 		<tr>
-    					<th> Название терминала </th>
-						<th> Описание </th>
-  					</tr>
-				</thead>
 				<NewTerminal terminal= {terminal} setTerminal={setTerminal}/>
 			</table>
 		</div>
